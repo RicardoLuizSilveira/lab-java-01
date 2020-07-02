d53c40a665a66ad6657f05d8259400b71b577284
Ricardo Luiz Silveira <ricardo.silveira@matera.com>
2020-07-01 23:44:38 -0300

    Unit tests

--- src/main/java/br/com/labjava/model/CandleStickFactory.java
+++ src/main/java/br/com/labjava/model/CandleStickFactory.java
@@ -6,17 +6,22 @@ import java.util.List;
 public class CandleStickFactory {
 
 	public CandleStick montaCandleStick(List<Negociacao> negociacoes, LocalDateTime data) {
+
+		if (negociacoes.isEmpty()) {
+			return new CandleStick(0.0, 0.0, 0.0, 0.0, 0.0, data);
+		}
+
 		Double abertura = negociacoes.get(0).getPreco();
-		Double fechamento = negociacoes.get(negociacoes.size() -1).getPreco();
+		Double fechamento = negociacoes.get(negociacoes.size() - 1).getPreco();
 		Double maximo = abertura;
 		Double minimo = abertura;
 		Double volume = 0.0;
-		
+
 		for (Negociacao negociacao : negociacoes) {
 			volume += negociacao.getVolume();
 			if (negociacao.getPreco() > maximo) {
 				maximo = negociacao.getPreco();
-			}else if (negociacao.getPreco() < minimo) {
+			} else if (negociacao.getPreco() < minimo) {
 				minimo = negociacao.getPreco();
 			}
 		}
---
+++ src/test/java/br/com/labjava/model/CandleStickTest.java
@@ -0,0 +1,97 @@
+package br.com.labjava.model;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class CandleStickTest {
+
+	@Test
+	public void testBasicoVariasNegociacoes() {
+		LocalDateTime data = LocalDateTime.now();
+		Negociacao n1 = new Negociacao(40.0, 10, data);
+		Negociacao n2 = new Negociacao(20.0, 10, data);
+		Negociacao n3 = new Negociacao(10.0, 10, data);
+		Negociacao n4 = new Negociacao(50.0, 10, data);
+		List<Negociacao> negociacoes = Arrays.asList(n1, n2, n3, n4);
+		
+		CandleStickFactory factory = new CandleStickFactory();
+		CandleStick candleStick = factory.montaCandleStick(negociacoes, data);
+		
+		assertEquals(40.0, candleStick.getAbertura(), 0.0001);
+		assertEquals(50.0, candleStick.getFechamento(), 0.0001);
+		assertEquals(50.0, candleStick.getMaximo(), 0.0001);
+		assertEquals(10.0, candleStick.getMinimo(), 0.0001);
+		assertEquals(1200.0, candleStick.getVolume(), 0.0001);
+		
+	}
+	
+	@Test
+	public void testUmaUnicaNegociacao() {
+		LocalDateTime data = LocalDateTime.now();
+		Negociacao n1 = new Negociacao(40.0, 10, data);
+		List<Negociacao> negociacoes = Arrays.asList(n1);
+		
+		CandleStickFactory factory = new CandleStickFactory();
+		CandleStick candleStick = factory.montaCandleStick(negociacoes, data);
+		
+		assertEquals(40.0, candleStick.getAbertura(), 0.0001);
+		assertEquals(40.0, candleStick.getFechamento(), 0.0001);
+		assertEquals(40.0, candleStick.getMaximo(), 0.0001);
+		assertEquals(40.0, candleStick.getMinimo(), 0.0001);
+		assertEquals(400.0, candleStick.getVolume(), 0.0001);
+		
+	}
+	
+	@Test
+	public void testZeroNegociacoes() {
+		LocalDateTime data = LocalDateTime.now();
+		List<Negociacao> negociacoes = new ArrayList<Negociacao>();
+		
+		CandleStickFactory factory = new CandleStickFactory();
+		CandleStick candleStick = factory.montaCandleStick(negociacoes, data);
+		
+		assertEquals(0.0, candleStick.getAbertura(), 0.0001);
+		assertEquals(0.0, candleStick.getFechamento(), 0.0001);
+		assertEquals(0.0, candleStick.getMaximo(), 0.0001);
+		assertEquals(0.0, candleStick.getMinimo(), 0.0001);
+		assertEquals(0.0, candleStick.getVolume(), 0.0001);
+		
+		assertTrue(!candleStick.isAlta());
+		assertTrue(!candleStick.isBaixa());
+		
+	}
+	
+	@Test
+	public void testCandlestickDeAlta() {
+		LocalDateTime data = LocalDateTime.now();
+		Negociacao n1 = new Negociacao(20.0, 10, data);
+		Negociacao n2 = new Negociacao(40.0, 10, data);
+		List<Negociacao> negociacoes = Arrays.asList(n1, n2);
+		
+		CandleStickFactory factory = new CandleStickFactory();
+		CandleStick candleStick = factory.montaCandleStick(negociacoes, data);
+		
+		assertTrue("O candlestick não era de alta", candleStick.isAlta());
+		
+	}
+	
+	@Test
+	public void testCandlestickDeBaixa() {
+		LocalDateTime data = LocalDateTime.now();
+		Negociacao n1 = new Negociacao(20.0, 10, data);
+		Negociacao n2 = new Negociacao(10.0, 10, data);
+		List<Negociacao> negociacoes = Arrays.asList(n1, n2);
+		
+		CandleStickFactory factory = new CandleStickFactory();
+		CandleStick candleStick = factory.montaCandleStick(negociacoes, data);
+		
+		assertTrue("O candlestick não era de baixa", candleStick.isBaixa());
+		
+	}
+}
